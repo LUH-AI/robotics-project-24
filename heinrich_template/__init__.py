8be94dfc2dd6f92766105ba44b29143c2b33d5a3
Theresa Eimer <t.eimer@ai.uni-hannover.de>
2024-07-09 12:01:21 +0200

    init repo

---
+++ heinrich_template/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for heinrich-template."""
+import datetime
+
+__author__ = """LUHAI Robots"""
+__email__ = """t.eimer@ai.uni-hannover.de"""
+__version__ = """0.1.0"""
+__copyright__ = f"Copyright {datetime.date.today().strftime('%Y')}, LUHAI Robots"
---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+"""Unit test package for heinrich_template."""
