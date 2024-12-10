b6768509fea92c32ae118b962e9752b2755f195a
Peer222 <pebadu@gmail.com>
2024-12-10 15:00:57 +0100

    empty room scene v1

--- simulation/configs/scenes/base.py
+++ simulation/configs/scenes/base.py
@@ -1,5 +1,43 @@
-from abc import ABC
+from typing import List, Literal, Tuple, Optional
+import typing
+from pathlib import Path
 
-class BaseSceneCfg(ABC):
-    name = ...
-    #TODO
+import torch
+from isaacgym import gymapi
+
+
+StaticObjectType = Literal["robot", "ground", "wall", "flower_pot", "ball", "obstacle"]
+Location = Tuple[float, float, float]
+
+
+class StaticObject():
+    def __init__(self, name: str, type: StaticObjectType, asset_path: Path, init_location: Optional[Location] = None, max_random_loc_offset: Optional[Location] = None) -> None:
+        """Create a static object
+
+        Args:
+            name (str): Name of object
+            type (StaticObjectType): Type of static object (More types can be appended if needed)
+            asset_path (Path): Path to urdf file
+            init_location (Optional[Location]): new x, y, z coordinates for placement of object in environment
+            max_random_loc_offset (Optional[Location]): maximal random absolute offset from x, y, z coordinates of init_location for environment/object initialization/reset
+        """
+        self.name: str = name
+        self.type = type
+        self.init_location = torch.tensor(init_location) if init_location != None else torch.zeros(3)
+        self.max_random_loc_offset = torch.tensor(max_random_loc_offset) if max_random_loc_offset != None else torch.zeros(3)
+
+        self.asset_path = asset_path
+        self.asset_root = asset_path.parent
+        self.asset_file = asset_path.name
+
+        self.asset_options = gymapi.AssetOptions()
+        self.asset_options.collapse_fixed_joints = True
+
+        types = typing.get_args(StaticObjectType)
+        self.segmentation_id: int = types.index(type)
+
+class BaseSceneCfg():
+    name: str = "ground_plane"
+    static_objects: List[StaticObject] = []
+    #initial_robot_position: Location = ...
+    size: float = 0.
--- simulation/task.py
+++ simulation/task.py
@@ -1,29 +1,80 @@
+from typing import Any
+
+import torch
+from isaacgym import gymapi
+
 from legged_gym.utils.task_registry import task_registry
-from legged_gym.envs.base.legged_robot import LeggedRobot
+
+from configs.robots import GO2DefaultCfg
+from configs.scenes import BaseSceneCfg
+from configs.algorithms import PPODefaultCfg
+from compatible_legged_robot import CompatibleLeggedRobot
 
 
-class CustomLeggedRobot(LeggedRobot):
-    # add custom rewards... here (use robot_cfg for control)
-    pass
+# do CONFIGURABLE adaptations in this file
 
 
 # register all tasks derived from CustomLeggedRobot
-def register_task(robot_cfg, scene_cfg, algorithm_cfg) -> str:
-    """Register task based on robot, scene and algorithms configurations
+def register_task(robot_cfg: GO2DefaultCfg, scene_cfg: BaseSceneCfg, algorithm_cfg: PPODefaultCfg) -> str:
+    """Register task based on robot, scene and algorithm configurations
     All configs need a name attribute.
 
     Args:
-        robot_cfg (_type_): Robot config
-        scene_cfg (_type_): Scene config
-        algorithm_cfg (_type_): Algorithm config
+        robot_cfg (GO2DefaultCfg): Robot config
+        scene_cfg (BaseSceneCfg): Scene config
+        algorithm_cfg (PPODefaultCfg): Algorithm config
 
     Returns:
         str: Task name
     """
     name = f"{robot_cfg.name}_{scene_cfg.name}_{algorithm_cfg.name}"
-    # add 
+
+    # add scene_cfg to robot_cfg for compatibility with task_registry
     robot_cfg.scene = scene_cfg
+    robot_cfg.env.env_spacing = scene_cfg.size
     algorithm_cfg.runner.experiment_name = name
     task_registry.register(name, CustomLeggedRobot, robot_cfg, algorithm_cfg)
     return name
-    #task_registry.register( "go2_default", CustomLeggedRobot, configs.GO2RoughCfg(), configs.GO2RoughCfgPPO())
+
+
+class CustomLeggedRobot(CompatibleLeggedRobot):
+    def __init__(self, cfg: GO2DefaultCfg, sim_params, physics_engine, sim_device, headless):
+        super().__init__(cfg, sim_params, physics_engine, sim_device, headless)
+        # for language server purposes only
+        self.cfg: GO2DefaultCfg = self.cfg
+
+    def _create_ground_plane(self):
+        """ Adds a ground plane to the simulation, sets friction and restitution based on the cfg.
+        Additionally, sets segmentation_id to 1 (index of ObjectType="ground")
+        """
+        plane_params = gymapi.PlaneParams()
+        plane_params.normal = gymapi.Vec3(0.0, 0.0, 1.0)
+        plane_params.static_friction = self.cfg.terrain.static_friction
+        plane_params.dynamic_friction = self.cfg.terrain.dynamic_friction
+        plane_params.restitution = self.cfg.terrain.restitution
+        plane_params.segmentation_id = 1 # added for compatibility
+        self.gym.add_ground(self.sim, plane_params)
+
+    def _place_static_objects(self, env_idx: int, env_handle: Any):
+        """Places static objects like walls into the provided environment
+
+        Args:
+            env_idx (int): Index of environment
+            env_handle (Any): Environment handle
+        """
+        self.object_handles.append([])
+        self.num_static_objects = len(self.cfg.scene.static_objects)
+        for static_obj in self.cfg.scene.static_objects:
+            obj_asset = self.gym.load_asset(self.sim, str(static_obj.asset_root), str(static_obj.asset_file), static_obj.asset_options)
+
+            start_pose = gymapi.Transform()
+            location_offset = self.env_origins[env_idx].clone()
+            init_location = static_obj.init_location.to(self.device)
+            random_loc_offset = (static_obj.max_random_loc_offset * (torch.rand(static_obj.max_random_loc_offset.shape) - 0.5) * 2).to(self.device)
+            start_pose.p = gymapi.Vec3(*(init_location + location_offset + random_loc_offset))
+
+            # env_idx sets collision group, -1 default for collision_filter
+            object_handle = self.gym.create_actor(env_handle, obj_asset, start_pose, static_obj.name, env_idx, -1, static_obj.segmentation_id)
+            self.object_handles[env_idx].append(object_handle)
+
+    # add custom rewards... here (use your robot_cfg for control)
