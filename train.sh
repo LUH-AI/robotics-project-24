2aa999c3a29b49225920668dda540dc11b44290a
Peer222 <pebadu@gmail.com>
2025-01-27 22:14:37 +0100

    bugfix: run_name

--- train.sh
+++ train.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-#python -m training_code_isaacgym.train --algorithm "ppo_move-policy_plant" --experiment "move_policy_plant" --robot_class "go2_default_class" --robot "go2_low-level-policy"
-python -m training_code_isaacgym.train --algorithm "ppo_high-level-policy_plant" --experiment "high_level_policy_plant" --robot_class "go2_high-level-policy_plant_class" --robot "go2_high-level-policy_plant" --algorithm "ppo_high-level-policy_plant"
+#python -m training_code_isaacgym.train --algorithm "ppo_move-policy_plant" --run_name "move_policy_plant" --robot_class "go2_default_class" --robot "go2_low-level-policy"
+python -m training_code_isaacgym.train --algorithm "ppo_high-level-policy_plant" --run_name "high_level_policy_plant" --robot_class "go2_high-level-policy_plant_class" --robot "go2_high-level-policy_plant" --scene single_plant
